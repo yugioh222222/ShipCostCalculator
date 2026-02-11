e5201f7ea05126f7f63f1c1ad3c81c885d058ca6
caleb <wambsgcc@mail.uc.edu>
2026-02-11 12:53:53 -0500

    Initial commit

---
+++ src/Lab5_ShipCostCalculator.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class Lab5_ShipCostCalculator {
+    public static void main(String[] args)
+        {
+            double itemPrice = 0;
+            double shipPrice = 0;
+            double finalPrice = 0;
+            Scanner in = new Scanner(System.in);
+            System.out.print("\nPlease enter your items price. ");
+            if( in.hasNextDouble())
+            {
+                itemPrice = in.nextDouble();
+                if (itemPrice <=0){
+                System.out.println("You gave a negative price. That is invalid");
+                return;
+            }
+
+                in.nextLine();
+                System.out.println("Your price is: " + itemPrice);
+
+            }
+            else{
+
+            System.out.println("You gave a invalid price");
+                return;
+
+
+        }
+            if (itemPrice >= 100){
+                System.out.println("Your will not have any sales tax so your total will be " + itemPrice);
+            }else{
+                shipPrice = itemPrice * .02;
+                finalPrice = itemPrice + shipPrice;
+                System.out.println("The shipping cost is " + shipPrice + " so your grand total is " + finalPrice);
+
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+}
