98617ee5f3ccce3364677686e3e1b7cfafc3ad1c
caleb <wambsgcc@mail.uc.edu>
2026-02-13 10:37:17 -0500

    True Final

--- src/Lab5_ShipCostCalculator.java
+++ src/Lab5_ShipCostCalculator.java
@@ -12,8 +12,8 @@ public class Lab5_ShipCostCalculator {
             {
                 itemPrice = in.nextDouble();
                 if (itemPrice <=0){
-                System.out.println("You gave a negative price. That is invalid");
-                return;
+                System.out.print("You gave a negative price. That is invalid");
+                    System.exit(0);
             }
 
                 in.nextLine();
@@ -22,8 +22,8 @@ public class Lab5_ShipCostCalculator {
             }
             else{
 
-            System.out.println("You gave a invalid price");
-                return;
+            System.out.print("You gave a invalid price");
+                System.exit(0);
 
 
         }
@@ -31,6 +31,7 @@ public class Lab5_ShipCostCalculator {
                 System.out.println("Your will not have any sales tax so your total will be " + itemPrice);
             }else{
                 shipPrice = itemPrice * .02;
+
                 finalPrice = itemPrice + shipPrice;
                 System.out.println("The shipping cost is " + shipPrice + " so your grand total is " + finalPrice);
 
